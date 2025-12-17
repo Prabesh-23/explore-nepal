194602e3c97911fd6ee3e9317f7f1b676ee6b16b
Prabesh Bhandari <prabeshbhandari52@gmail.com>
2025-12-17 12:32:48 +0545

    Added 3 more tourist destinations: Chitwan, Illam, Janakpur

--- src/data/destinations.js
+++ src/data/destinations.js
@@ -1,22 +1,45 @@
+// Import all images
+import chitwan from "../assets/images/chitwan.jpg";
+import ilam from "../assets/images/illam.jpg";
+import janakpur from "../assets/images/janakpur.jpg";
 import kathmandu from "../assets/images/kathmandu.jpg";
 import lumbini from "../assets/images/lumbini.jpg";
 import pokhara from "../assets/images/pokhara.jpg";
 
+// Destinations array
 const destinations = [
   {
     id: 1,
+    name: "Chitwan",
+    description: "Home to Chitwan National Park and wildlife safaris.",
+    image: chitwan,
+  },
+  {
+    id: 2,
+    name: "Illam",
+    description: "Famous for lush tea gardens and scenic hills.",
+    image: ilam,
+  },
+  {
+    id: 3,
+    name: "Janakpur",
+    description: "Known for Janaki Temple and rich cultural heritage.",
+    image: janakpur,
+  },
+  {
+    id: 4,
     name: "Kathmandu",
     description: "Capital city rich in culture and heritage.",
     image: kathmandu,
   },
   {
-    id: 2,
+    id: 5,
     name: "Lumbini",
     description: "Birthplace of Lord Buddha.",
     image: lumbini,
   },
   {
-    id: 3,
+    id: 6,
     name: "Pokhara",
     description: "City of lakes and gateway to Annapurna.",
     image: pokhara,
